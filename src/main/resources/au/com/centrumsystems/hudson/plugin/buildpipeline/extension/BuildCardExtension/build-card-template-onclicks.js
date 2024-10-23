913d01a13f6d9fa1e817818c336fb7e870ac5298
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-10-23 16:54:15 -0400

    line 97: https://github.com/jenkinsci/build-pipeline-plugin/blob/master/src/main/resources/au/com/centrumsystems/hudson/plugin/buildpipeline/extension/BuildCardExtension/buildCardTemplate.jelly\#L97

--- src/main/resources/au/com/centrumsystems/hudson/plugin/buildpipeline/extension/BuildCardExtension/build-card-template-onclicks.js
+++ src/main/resources/au/com/centrumsystems/hudson/plugin/buildpipeline/extension/BuildCardExtension/build-card-template-onclicks.js
@@ -1,10 +1,21 @@
 Behaviour.specify(".progress-console-onclick", 'progress-bar-click', 0, function (progressBarElement) {
 
-    const dataContainer = document.querySelector(".fill-dialog-data-container");
+    const dataContainer = document.querySelector(".fill-dialog-params-statusbar");
     const href = dataContainer.dataset.fillDialogHref;
     const title = dataContainer.dataset.fillDialogTitle;
 
     progressBarElement.addEventListener('click', () => {
+        buildPipeline.fillDialog(href, title);
+    });
+});
+
+Behaviour.specify(".console-icon-onlick", 'console-icon-click', 0, function (consoleIconElement) {
+
+    const dataContainer = document.querySelector(".fill-dialog-params-console-output-icon");
+    const href = dataContainer.dataset.fillDialogHref;
+    const title = dataContainer.dataset.fillDialogTitle;
+
+    consoleIconElement.addEventListener('click', () => {
         buildPipeline.fillDialog(href, title);
     });
 });
