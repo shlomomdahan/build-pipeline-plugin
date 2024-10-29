31008b15489f09e2429733674786f553b1bc9bb7
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-10-29 15:50:57 -0400

    fix: https://github.com/jenkinsci/build-pipeline-plugin/blob/master/src/main/resources/au/com/centrumsystems/hudson/plugin/buildpipeline/extension/BuildCardExtension/buildCardTemplate.jelly\#L109

--- src/main/resources/au/com/centrumsystems/hudson/plugin/buildpipeline/extension/BuildCardExtension/build-card-template-onclicks.js
+++ src/main/resources/au/com/centrumsystems/hudson/plugin/buildpipeline/extension/BuildCardExtension/build-card-template-onclicks.js
@@ -1,21 +1,39 @@
-Behaviour.specify(".progress-console-onclick", 'progress-bar-click', 0, function (progressBarElement) {
+Behaviour.specify(".bct-progress-console-onclick", 'BuildCardExtension_progressBarClick', 0, function (element) {
 
-    const dataContainer = document.querySelector(".fill-dialog-params-statusbar");
+    const dataContainer = document.querySelector(".bct-progress-console-onclick-params");;
     const href = dataContainer.dataset.fillDialogHref;
     const title = dataContainer.dataset.fillDialogTitle;
 
-    progressBarElement.addEventListener('click', () => {
+    element.addEventListener('click', () => {
         buildPipeline.fillDialog(href, title);
     });
 });
 
-Behaviour.specify(".console-icon-onlick", 'console-icon-click', 0, function (consoleIconElement) {
+Behaviour.specify(".bct-console-icon-onclick", 'BuildCardExtension_consoleIconClick', 0, function (element) {
 
-    const dataContainer = document.querySelector(".fill-dialog-params-console-output-icon");
+    const dataContainer = document.querySelector(".bct-console-icon-onclick-params");
     const href = dataContainer.dataset.fillDialogHref;
     const title = dataContainer.dataset.fillDialogTitle;
 
-    consoleIconElement.addEventListener('click', () => {
+    element.addEventListener('click', () => {
         buildPipeline.fillDialog(href, title);
     });
 });
+
+Behaviour.specify(".bct-rerun-success-icon-onclick", 'BuildCardExtension_successRerunClick', 0, function(element) {
+
+    const dataContainer = document.querySelector(".bct-rerun-success-icon-onclick-params");
+    const id = parseInt(dataContainer.dataset.showSpinnerId, 10);
+    const buildExtId = dataContainer.dataset.rerunBuildExtId;
+    const dependencyIds = dataContainer.dataset.rerunBuildDependencyIds
+        .split(',')
+        .filter(Boolean)
+        .map(id => parseInt(id, 10));
+
+    element.addEventListener('click', () => {
+        buildPipeline.showSpinner(id);
+        buildPipeline.rerunBuild(id, buildExtId, dependencyIds);
+    });
+});
+
+
