b5ccdd49910cb8a6490dbcac681be09c1affe0a0
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-10-30 18:38:47 -0400

    Extract event handlers in

---
+++ src/main/resources/au/com/centrumsystems/hudson/plugin/buildpipeline/BuildPipelineView/bpp-trigger-pipeline.js
@@ -0,0 +1,17 @@
+Behaviour.specify(".bpp-trigger-pipeline-parameterized", 'BuildPipelineView.TriggerPipelineParameterized', 0, function(element) {
+    element.addEventListener('click', function() {
+
+        const paramsElement = element.previousElementSibling;
+        const href = paramsElement.dataset.fillDialogHref;
+        const title = paramsElement.dataset.fillDialogTitle;
+
+        buildPipeline.fillDialog(href, title);
+
+    });
+});
+
+Behaviour.specify(".bpp-trigger-pipeline-button", 'BuildPipelineView.TriggerPipeline', 0, function(element) {
+    element.addEventListener('click', function(e) {
+        document.getElementById('triggerPipelineForm').submit();
+    });
+});
