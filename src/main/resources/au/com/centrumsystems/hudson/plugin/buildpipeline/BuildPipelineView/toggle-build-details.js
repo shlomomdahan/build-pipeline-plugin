85e53a361834d11bc2d16505f5ce6333051b22a7
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-10-31 18:16:20 -0400

    extract inmline JS in main_dashboard.jelly

---
+++ src/main/resources/au/com/centrumsystems/hudson/plugin/buildpipeline/BuildPipelineView/toggle-build-details.js
@@ -0,0 +1,10 @@
+// show/hide build details
+jQuery(function() {
+    jQuery(".header").click(function() {
+        var parent = jQuery(this).parent();
+        var ba = parent.find(".build-actions");
+        var bb = parent.find(".build-body");
+
+        ba.add(bb).toggle('slow');
+    });
+});
