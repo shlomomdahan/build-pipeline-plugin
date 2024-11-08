cfae875a09d00e0eca79747ca3853f865bd066c0
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-11-08 15:45:26 -0500

    remove redundant span, add attributes to trigger span

--- src/main/resources/au/com/centrumsystems/hudson/plugin/buildpipeline/BuildPipelineView/bpp-trigger-pipeline.js
+++ src/main/resources/au/com/centrumsystems/hudson/plugin/buildpipeline/BuildPipelineView/bpp-trigger-pipeline.js
@@ -1,8 +1,7 @@
 Behaviour.specify(".bpp-trigger-pipeline-parameterized", 'BuildPipelineView.TriggerPipelineParameterized', 0, function(element) {
     element.addEventListener('click', function() {
-        const paramsElement = document.querySelector('.bpp-trigger-pipeline-parameterized-params');
-        const href = paramsElement.dataset.fillDialogHref;
-        const title = paramsElement.dataset.fillDialogTitle;
+        const href = element.dataset.fillDialogHref;
+        const title = element.dataset.fillDialogTitle;
         buildPipeline.fillDialog(href, title);
     });
 });
