7d759f12ca8a444affd5740d437b324fb1087267
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-11-11 16:49:29 -0500

    remove redundant ID. Show tooltip when hovering card body

--- src/main/resources/au/com/centrumsystems/hudson/plugin/buildpipeline/extension/AbstractNameValueHeader/build-parameters-tooltip.js
+++ src/main/resources/au/com/centrumsystems/hudson/plugin/buildpipeline/extension/AbstractNameValueHeader/build-parameters-tooltip.js
@@ -1,5 +1,5 @@
 const buildId = document.querySelector('.pipeline-info .revision .title').dataset.buildId;
-jQuery('#build-parameters-trigger-' + buildId).tooltip({
+jQuery('#build-parameters-' + buildId).tooltip({
     bodyHandler: function() {
         return jQuery('#build-parameters-' + buildId).html();
     },
