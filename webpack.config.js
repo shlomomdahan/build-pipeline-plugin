022d2bf435e2fef060893a0f496bcaf1ff40e0f3
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-11-11 12:31:11 -0500

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,11 @@ const path = require('path');
 
 module.exports = {
     entry: './src/main/webapp/templates/index.js',
-    devtool: 'source-map',
     module: {
         rules: [
             {
                 test: /\.hbs$/,
                 loader: 'handlebars-loader',
-                options: {
-                    recompileOptions: {
-                        knownHelpersOnly: false,
-                        strict: false,
-                    },
-                },
             }
         ]
     },
